1449d671dafd9be427d1a80f78b1d6d54644375b
AaronStGeorge <aaronstgeorge@gmail.com>
2017-08-14 08:01:55 -0600

    CamelCase is not acceptable (for rust programs).

--- src/parser/core.rs
+++ src/parser/core.rs
@@ -4,8 +4,8 @@ use lexer::*;
 pub struct Parser {
     lexer: Lexer,
 
-    currToken: Option<Token>,
-    peekToken: Option<Token>,
+    curr_token: Option<Token>,
+    peek_token: Option<Token>,
 }
 
 // TODO: this should use the same method names as the lexer
@@ -14,8 +14,8 @@ impl Parser {
         let mut parser = Parser {
             lexer: Lexer::new(input),
 
-            currToken: None,
-            peekToken: None,
+            curr_token: None,
+            peek_token: None,
         };
 
         parser.advance();
@@ -26,11 +26,11 @@ impl Parser {
 
     // This should probably just be done with indexes
     fn advance(&mut self) {
-        self.currToken = self.peekToken.clone();
-        self.peekToken = self.lexer.next();
+        self.curr_token = self.peek_token.clone();
+        self.peek_token = self.lexer.next();
     }
 
-    fn parseProgram() -> Option<Program> {
+    fn parse_program() -> Option<Program> {
         None
     }
 }
