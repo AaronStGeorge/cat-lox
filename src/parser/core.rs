5abc20e671183899e3e0cd529e1e5ef4b06088cf
AaronStGeorge <aaronstgeorge@gmail.com>
2017-09-07 21:15:48 -0600

    Start on parser.

--- src/parser/core.rs
+++ src/parser/core.rs
@@ -2,35 +2,56 @@ use ast::*;
 use lexer::*;
 
 pub struct Parser {
-    lexer: Lexer,
-
-    curr_token: Option<Token>,
-    peek_token: Option<Token>,
+    tokens: Vec<Token>,
+    index: usize,
 }
 
-// // TODO: this should use the same method names as the lexer
-// impl Parser {
-//     pub fn new(input: &str) -> Parser {
-//         let mut parser = Parser {
-//             lexer: Lexer::new(input),
+impl Parser {
+    pub fn new(input: &str) -> Parser {
+        return Parser {
+            tokens: Lexer::new(input).collect(),
+            index: 0,
+        };
+    }
 
-//             curr_token: None,
-//             peek_token: None,
-//         };
+    // This should probably just be done with indexes
+    fn advance(&mut self) -> Option<&Token> {
+        if self.index >= self.tokens.len() {
+            None
+        } else {
+            self.index += 1;
+            Some(&self.tokens[self.index - 1])
+        }
+    }
 
-//         parser.advance();
-//         parser.advance();
+    fn peek(&self) -> Option<&Token> {
+        if self.index >= self.tokens.len() {
+            None
+        } else {
+            Some(&self.tokens[self.index])
+        }
+    }
 
-//         parser
-//     }
+    fn expression(&mut self) -> Expression {
+        self.equality()
+    }
 
-//     // This should probably just be done with indexes
-//     fn advance(&mut self) {
-//         self.curr_token = self.peek_token.clone();
-//         self.peek_token = self.lexer.next();
-//     }
+    fn equality(&mut self) -> Expression {
+        let mut expr = self.comparison();
 
-//     fn parse_program() -> Option<Program> {
-//         None
-//     }
-// }
+        while let Some(t) = match self.peek() {
+            Some(t) if *t == Token::Equal || *t == Token::NotEqual => Some(t.clone()),
+            _ => None,
+        } {
+            self.advance();
+            let right = self.comparison();
+            expr = Expression::Binary(Box::new(expr), Box::new(t), Box::new(right));
+        }
+
+        expr
+    }
+
+    fn comparison(&mut self) -> Expression {
+        Expression::Literal("1".to_string())
+    }
+}
