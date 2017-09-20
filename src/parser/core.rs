d64dcb5e8a5554a0efafbbca41b8918e9d7cf4dc
AaronStGeorge <aaronstgeorge@gmail.com>
2017-09-19 18:59:58 -0600

    Use Cell to simplify  match conditions.

--- src/parser/core.rs
+++ src/parser/core.rs
@@ -1,16 +1,17 @@
 use ast::*;
 use lexer::*;
+use std::cell::Cell;
 
 pub struct Parser<'a> {
     tokens: &'a [Token],
-    index: usize,
+    index: Cell<usize>,
 }
 
 impl<'a> Parser<'a> {
     pub fn new(input: &'a [Token]) -> Parser {
         return Parser {
             tokens: input,
-            index: 0,
+            index: Cell::new(0),
         };
     }
 
@@ -18,41 +19,42 @@ impl<'a> Parser<'a> {
         self.expression()
     }
 
-    fn advance(&mut self) -> Option<&Token> {
-        if self.index >= self.tokens.len() {
+    fn advance(&self) -> Option<&Token> {
+        let previous_index = self.index.get();
+        if previous_index >= self.tokens.len() {
             None
         } else {
-            self.index += 1;
-            Some(&self.tokens[self.index - 1])
+            self.index.set(previous_index + 1);
+            Some(&self.tokens[previous_index])
         }
     }
 
     fn peek(&self) -> Option<&Token> {
-        if self.index >= self.tokens.len() {
+        let index = self.index.get();
+        if index >= self.tokens.len() {
             None
         } else {
-            Some(&self.tokens[self.index])
+            Some(&self.tokens[index])
         }
     }
 
-    fn expression(&mut self) -> Result<Expression, &'static str> {
+    fn expression(&self) -> Result<Expression, &'static str> {
         self.equality()
     }
 
-    fn equality(&mut self) -> Result<Expression, &'static str> {
+    fn equality(&self) -> Result<Expression, &'static str> {
         let mut expr = self.comparison()?;
 
         while let Some(t) = match self.peek() {
             Some(t) if *t == Token::Equal || *t == Token::NotEqual => {
-                Some(t.clone())
+                self.advance()
             }
             _ => None,
         } {
-            self.advance();
             let right = self.comparison()?;
             expr = Expression::Binary(
                 Box::new(expr),
-                Box::new(t),
+                Box::new(t.clone()),
                 Box::new(right),
             );
         }
@@ -60,7 +62,7 @@ impl<'a> Parser<'a> {
         Ok(expr)
     }
 
-    fn comparison(&mut self) -> Result<Expression, &'static str> {
+    fn comparison(&self) -> Result<Expression, &'static str> {
         let mut expr = self.addition()?;
 
         while let Some(t) = match self.peek() {
@@ -69,15 +71,14 @@ impl<'a> Parser<'a> {
                     *t == Token::LessThan ||
                     *t == Token::LessEqual =>
             {
-                Some(t.clone())
+                self.advance()
             }
             _ => None,
         } {
-            self.advance();
             let right = self.addition()?;
             expr = Expression::Binary(
                 Box::new(expr),
-                Box::new(t),
+                Box::new(t.clone()),
                 Box::new(right),
             );
         }
@@ -85,20 +86,19 @@ impl<'a> Parser<'a> {
         Ok(expr)
     }
 
-    fn addition(&mut self) -> Result<Expression, &'static str> {
+    fn addition(&self) -> Result<Expression, &'static str> {
         let mut expr = self.multiplication()?;
 
         while let Some(t) = match self.peek() {
             Some(t) if *t == Token::Minus || *t == Token::Plus => {
-                Some(t.clone())
+                self.advance()
             }
             _ => None,
         } {
-            self.advance();
             let right = self.multiplication()?;
             expr = Expression::Binary(
                 Box::new(expr),
-                Box::new(t),
+                Box::new(t.clone()),
                 Box::new(right),
             );
         }
@@ -106,20 +106,19 @@ impl<'a> Parser<'a> {
         Ok(expr)
     }
 
-    fn multiplication(&mut self) -> Result<Expression, &'static str> {
+    fn multiplication(&self) -> Result<Expression, &'static str> {
         let mut expr = self.unary()?;
 
         while let Some(t) = match self.peek() {
             Some(t) if *t == Token::Slash || *t == Token::Asterisk => {
-                Some(t.clone())
+                self.advance()
             }
             _ => None,
         } {
-            self.advance();
             let right = self.unary()?;
             expr = Expression::Binary(
                 Box::new(expr),
-                Box::new(t),
+                Box::new(t.clone()),
                 Box::new(right),
             );
         }
@@ -127,54 +126,40 @@ impl<'a> Parser<'a> {
         Ok(expr)
     }
 
-    fn unary(&mut self) -> Result<Expression, &'static str> {
-        if let Some(t) = match self.peek() {
+    fn unary(&self) -> Result<Expression, &'static str> {
+        match self.peek() {
             Some(t) if *t == Token::Bang || *t == Token::Minus => {
-                Some(t.clone())
+                self.advance();
+                let right = self.unary()?;
+                return Ok(
+                    Expression::Unary(Box::new(t.clone()), Box::new(right)),
+                );
             }
-            _ => None,
-        } {
-            self.advance();
-            let right = self.unary()?;
-            return Ok(Expression::Unary(Box::new(t), Box::new(right)));
+            _ => self.primary(),
         }
-
-        self.primary()
     }
 
-    fn primary(&mut self) -> Result<Expression, &'static str> {
-        // TODO: this is super ugly. It's here to be able to get an owned
-        // version of t and prevent a mutable reference to *self being borrowed
-        // for the scope of the whole function. Interior mutability primitives
-        // like Cell might be able to help here.
-        if let Some(t) = match self.advance() {
-            Some(t) => Some(t.clone()),
-            _ => None,
-        } {
-            match t {
-                Token::LeftParentheses => {
-                    let expr = self.expression()?;
+    fn primary(&self) -> Result<Expression, &'static str> {
+        match self.advance() {
+            Some(t) if *t == Token::LeftParentheses => {
+                let expr = self.expression()?;
 
-                    // Same grossness if self.advance didn't need to take a
-                    // mutable reference this could be a lot nicer.
-                    if let Some(_) = match self.advance() {
-                        Some(t) if *t == Token::RightParentheses => {
-                            Some(t.clone())
-                        }
-                        _ => None,
-                    } {
-                        Ok(Expression::Grouping(Box::new(expr)))
-                    } else {
-                        Err("There should be a fucking right parentheses here!")
-                    }
+                // Same grossness if self.advance didn't need to take a
+                // mutable reference this could be a lot nicer.
+                if let Some(_) = match self.advance() {
+                    Some(t) if *t == Token::RightParentheses => Some(t.clone()),
+                    _ => None,
+                } {
+                    Ok(Expression::Grouping(Box::new(expr)))
+                } else {
+                    Err("There should be a fucking right parentheses here!")
                 }
-                // TODO: this will match all tokens including things that don't
-                // make any sense here like ")". This should only accept a
-                // portion of what it does.
-                _ => Ok(Expression::Literal(Box::new(t.clone()))),
             }
-        } else {
-            Err("There should be some shit here!")
+            // TODO: this will match all tokens including things that don't
+            // make any sense here like ")". This should only accept a
+            // portion of what it does.
+            Some(t) => Ok(Expression::Literal(Box::new(t.clone()))),
+            None => Err("There should be some shit here!"),
         }
     }
 }
